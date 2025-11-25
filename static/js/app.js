abf266abc009fc95c62553ecad77d0ba2289e8cc
Matteovullo <matteovullo61@gmail.com>
2025-11-25 14:38:58 +0100

    script aggiornati

--- static/js/app.js
+++ static/js/app.js
@@ -1,7 +1,7 @@
 let currentToken = localStorage.getItem('cognitoToken');
 let currentUser = localStorage.getItem('cognitoUser');
 
-let API_BASE_URL = 'https://29hdihcwlj.execute-api.eu-west-1.amazonaws.com/Prod';
+let API_BASE_URL = 'https://d5hu2e3szi.execute-api.eu-west-1.amazonaws.com/Prod';
 
 function showMessage(text, type = 'info') {
     const messageDiv = document.getElementById('message') || createMessageDiv();
